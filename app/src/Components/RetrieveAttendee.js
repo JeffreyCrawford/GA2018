88750a98ec9859a8f4d6e62ea9d6514bf836f63e
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-10-16 13:22:09 -0400

    retrieval state improvements

--- app/src/Components/RetrieveAttendee.js
+++ app/src/Components/RetrieveAttendee.js
@@ -4,6 +4,11 @@ import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import { Typography } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+
 
 const styles = theme => ({
   root: {
@@ -37,7 +42,8 @@ class RetrieveAttendee extends React.Component {
 
     componentDidMount() {
     var _this = this;
-    fetch("/api/attendees/1", {
+    let id = 2;
+    fetch("/api/attendees/" + id, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
@@ -50,7 +56,21 @@ class RetrieveAttendee extends React.Component {
     .then(function(res) {
         console.log(res[0])
         _this.setState({
-            fullName: res[0].fullName
+            fullName: res[0].fullName,
+            firstName: res[0].firstName,
+            middleName: res[0].middleName,
+            lastName: res[0].lastName,
+            jobTitle: res[0].jobTitle,
+            account: res[0].account,
+            countyAccountAccount: res[0].countyAccountAccount,
+            rsvpGa2018: res[0].rsvpGa2018,
+            proxyDesigneeGa2018: res[0].proxyDesigneeGa2018,
+            ga2018AsADesigneeFor: res[0].ga2018AsADesigneeFor,
+            nopecGeneralAssemblyMember: res[0].nopecGeneralAssemblyMember,
+            accountTypeAccountAccount: res[0].accountTypeAccountAccount,
+            gaDelegateAccountAccount: res[0].gaDelegateAccountAccount,
+            checkInTime: res[0].checkInTime,
+            badge: res[0].badge
         }) 
     });
   }
@@ -61,19 +81,15 @@ class RetrieveAttendee extends React.Component {
 
   return (
     <div>
-      <Paper className={classes.root} elevation={1}>
-        <Typography>hello</Typography>
-
-                <TextField
-					id="fullName"
-					label="fullName"
-					className={classes.textField}
-					value={this.state.fullName}
-					style = {{width: 300}}
-					margin="normal"
-				/>
-            
-      </Paper>
+    <Card className={classes.card}>
+      <CardContent>
+        <Paper className={classes.root} elevation={1} margin={300}>
+            <Typography>
+                Welcome {this.state.jobTitle} {this.state.fullName}!
+            </Typography>
+        </Paper>
+        </CardContent>
+    </Card>
     </div>
   );
 }
